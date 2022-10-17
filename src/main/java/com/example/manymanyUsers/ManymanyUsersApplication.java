d92e00e3a0cc8f151c07a6dfc684b3d42b5d965b
chujonghyun <chuwhdgus@gmail.com>
2022-10-17 23:14:17 +0900

    test commit

---
+++ src/main/java/com/example/manymanyUsers/ManymanyUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.manymanyUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManymanyUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManymanyUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manymanyUsers/ManymanyUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manymanyUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManymanyUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
