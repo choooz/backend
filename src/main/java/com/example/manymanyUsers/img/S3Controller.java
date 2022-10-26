69401f18b7fe4ce4e3bf67f3d5bae847c62bfaa6
chujonghyun <chuwhdgus@gmail.com>
2022-10-26 12:45:17 +0900

    swagger 추가

--- src/main/java/com/example/manymanyUsers/img/S3Controller.java
+++ src/main/java/com/example/manymanyUsers/img/S3Controller.java
@@ -1,5 +1,9 @@
 package com.example.manymanyUsers.img;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +14,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "image", description = "S3 업로드 api")
 public class S3Controller {
     private final S3Uploader s3Uploader;
 
-    @PostMapping("/image")
-    public ResponseEntity<HttpStatus> updateUserImage(@RequestParam("images") MultipartFile multipartFile) {
+    @Operation(summary = "이미지 업로드", description = "이미지 업로드 메서드 params 이름으로 images 로 설정해주셔야 합니다.")
+    @Parameters({
+            @Parameter(name = "images", description = "이미지 파일", example = "image1.jpg")
+    })
+    @PostMapping("/api/image")
+    public ResponseEntity<HttpStatus> updateUserImage(
+            @RequestParam("images") MultipartFile multipartFile) {
         try {
             s3Uploader.uploadFiles(multipartFile, "static");
         } catch (Exception e) {
