0f743124e0d07b33e14a702e9a875e79ba49c754
chujonghyun <chuwhdgus@gmail.com>
2022-10-27 23:04:07 +0900

    RDS 연동 설정 추가

--- src/test/java/com/example/manymanyUsers/ManymanyUsersApplicationTests.java
+++ src/test/java/com/example/manymanyUsers/ManymanyUsersApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.manymanyUsers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ManymanyUsersApplicationTests {
 
 	@Test
