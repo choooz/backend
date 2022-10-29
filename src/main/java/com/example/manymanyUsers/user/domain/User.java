78537254e4a3cf246415fe5023aeb0a9e29067d0
chujonghyun <chuwhdgus@gmail.com>
2022-10-29 13:06:06 +0900

    baseTimeEntity 추가

---
+++ src/main/java/com/example/manymanyUsers/user/domain/User.java
@@ -0,0 +1,12 @@
+package com.example.manymanyUsers.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class User  {
+}
