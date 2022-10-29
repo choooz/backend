f92454697c5d33e7c161e8254cc20f5cfb960bfb
chujonghyun <chuwhdgus@gmail.com>
2022-10-29 13:54:54 +0900

    UserReposity 추가

--- src/main/java/com/example/manymanyUsers/user/domain/User.java
+++ src/main/java/com/example/manymanyUsers/user/domain/User.java
@@ -20,5 +20,8 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private String email;
+
     private String imageUrl;
 }
--- src/main/java/com/example/manymanyUsers/user/service/UserService.java
+++ src/main/java/com/example/manymanyUsers/user/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.manymanyUsers.user.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 }
