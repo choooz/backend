166e9b91cffa3eeab1bd382f986b2d038c7cf064
chujonghyun <chuwhdgus@gmail.com>
2022-11-08 13:29:08 +0900

    DefaultOAuth2User Class 생성

--- src/main/java/com/example/manymanyUsers/config/oauth2/UserOAuth2Service.java
+++ src/main/java/com/example/manymanyUsers/config/oauth2/UserOAuth2Service.java
@@ -1,4 +1,45 @@
 package com.example.manymanyUsers.config.oauth2;
 
-public class UserOAuth2Service {
+import com.example.manymanyUsers.user.domain.Role;
+import com.example.manymanyUsers.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserOAuth2Service extends DefaultOAuth2UserService {
+
+    private final HttpSession httpSession;
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
+        String email = (String) kakao_account.get("email");
+
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        String nickname = (String) properties.get("nickname");
+
+        if(userRepository.existsByEmail(email)){
+//            userRepository
+        }else{
+            System.out.println("이미 가입한 유저이므로 회원가입을 진행하지 않습니다.");
+        }
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(Role.ROLE_USER.name())), attributes, "id");
+    }
 }
