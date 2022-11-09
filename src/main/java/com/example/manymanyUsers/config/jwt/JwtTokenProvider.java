8c5a9bd3d9fb62fcc2abbabe16ffeba527e42b68
chujonghyun <chuwhdgus@gmail.com>
2022-11-09 10:34:00 +0900

    fixed untracked files

--- src/main/java/com/example/manymanyUsers/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/manymanyUsers/config/jwt/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.example.manymanyUsers.config.jwt;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -7,5 +8,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class JwtTokenProvider {
 
+    @Value("${jwt.access-token.expire-length}")
+    private Long accessTokenValidityInMilliseconds;
 
 }
