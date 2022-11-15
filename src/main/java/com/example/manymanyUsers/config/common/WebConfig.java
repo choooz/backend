d2e0cff9ed5642ff80ecc03b83d587df2027d40d
chujonghyun <chuwhdgus@gmail.com>
2022-11-15 22:30:50 +0900

    test commit

--- src/main/java/com/example/manymanyUsers/config/common/WebConfig.java
+++ src/main/java/com/example/manymanyUsers/config/common/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.manymanyUsers.config.common;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,4 +13,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*");
     }
+
 }
