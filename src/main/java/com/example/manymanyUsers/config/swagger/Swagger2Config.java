d1414547c7f0e3b625e36f6a3bf8bd7f5f9cc3a2
chujonghyun <chuwhdgus@gmail.com>
2022-11-15 21:10:34 +0900

    WebConfig 추가

--- src/main/java/com/example/manymanyUsers/config/swagger/Swagger2Config.java
+++ src/main/java/com/example/manymanyUsers/config/swagger/Swagger2Config.java
@@ -7,23 +7,23 @@ import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class Swagger2Config {
-
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("v1-definition")
-                .pathsToMatch("/api/**")
-                .build();
-    }
-    @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(new Info().title("ManyUser Project API")
-                        .description("프로젝트 API 명세서입니다.")
-                        .version("v0.0.1"));
-    }
-
-}
+//@Configuration
+//public class Swagger2Config {
+//
+//    @Bean
+//    public GroupedOpenApi publicApi() {
+//        return GroupedOpenApi.builder()
+//                .group("v1-definition")
+//                .pathsToMatch("/api/**")
+//                .build();
+//    }
+//    @Bean
+//    public OpenAPI springShopOpenAPI() {
+//        return new OpenAPI()
+//                .components(new Components())
+//                .info(new Info().title("ManyUser Project API")
+//                        .description("프로젝트 API 명세서입니다.")
+//                        .version("v0.0.1"));
+//    }
+//
+//}
