e877e51db2e0487747b0037a7a8866e36a8cac78
chujonghyun <chuwhdgus@gmail.com>
2022-11-26 23:47:08 +0900

    응답형태 수정

--- src/main/java/com/example/manymanyUsers/config/oauth2/kakao/controller/KakaoController.java
+++ src/main/java/com/example/manymanyUsers/config/oauth2/kakao/controller/KakaoController.java
@@ -2,10 +2,10 @@ package com.example.manymanyUsers.config.oauth2.kakao.controller;
 
 import com.example.manymanyUsers.config.oauth2.kakao.dto.GetUserInfo;
 import com.example.manymanyUsers.config.oauth2.kakao.dto.GetkakaoToken;
+import com.example.manymanyUsers.config.oauth2.kakao.dto.TokenResponse;
 import com.example.manymanyUsers.config.oauth2.kakao.service.KakaoService;
 import com.example.manymanyUsers.user.domain.User;
 import com.example.manymanyUsers.user.domain.UserRepository;
-import com.example.manymanyUsers.user.service.UserService;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
@@ -34,11 +34,16 @@ public class KakaoController {
      * @throws ParseException
      */
     @PostMapping("/kakao")
-    public ResponseEntity<String> getKaKaoToken(@Valid @RequestBody GetkakaoToken getkakaoToken) throws IOException, ParseException {
+    public ResponseEntity getKaKaoToken(@Valid @RequestBody GetkakaoToken getkakaoToken) throws IOException, ParseException {
         String code = getkakaoToken.getCode();
         String redirectUrl = getkakaoToken.getRedirectUrl();
         String accessToken = kakaoService.KakaoLogin(code, redirectUrl);
-        return ResponseEntity.ok(accessToken);
+//        return ResponseEntity.ok(accessToken);
+        TokenResponse tokenResponse = TokenResponse.builder()
+                .token(accessToken)
+                .message("엑세스 토큰")
+                .build();
+        return new ResponseEntity(tokenResponse, HttpStatus.OK);
     }
 
 
---
+++ src/main/java/com/example/manymanyUsers/config/oauth2/kakao/dto/TokenResponse.java
@@ -0,0 +1,21 @@
+package com.example.manymanyUsers.config.oauth2.kakao.dto;
+
+import com.example.manymanyUsers.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TokenResponse {
+    private String token;
+    private String message;
+
+    @Builder
+    public TokenResponse(String token, String message) {
+        this.token = token;
+        this.message = message;
+    }
+
+}
