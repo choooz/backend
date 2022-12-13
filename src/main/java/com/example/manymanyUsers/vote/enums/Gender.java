98dac1d1b00e4bf1c9e927d5997c022767ea7d6e
chujonghyun <chuwhdgus@gmail.com>
2022-12-13 13:35:15 +0900

    Gender 코드 추가

--- src/main/java/com/example/manymanyUsers/vote/enums/Gender.java
+++ src/main/java/com/example/manymanyUsers/vote/enums/Gender.java
@@ -1,6 +1,8 @@
 package com.example.manymanyUsers.vote.enums;
 
-public enum Gender {
+import com.example.manymanyUsers.common.enums.EnumModel;
+
+public enum Gender implements EnumModel {
     MALE("남자"),
     FEMALE("여자");
 
@@ -11,4 +13,21 @@ public enum Gender {
     }
 
 
+    /**
+     * enum key 리턴
+     * @return 'Male' or 'FEMALE'
+     */
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    /**
+     * enum value 리턴
+     * @return '남자' or '여자'
+     */
+    @Override
+    public String getValue() {
+        return value;
+    }
 }
