108e0c70f50ed2ed9cace6759a563ec0d596e562
chujonghyun <chuwhdgus@gmail.com>
2022-12-13 14:43:18 +0900

    category 코드 추가

--- src/main/java/com/example/manymanyUsers/vote/enums/Category.java
+++ src/main/java/com/example/manymanyUsers/vote/enums/Category.java
@@ -1,4 +1,24 @@
 package com.example.manymanyUsers.vote.enums;
 
-public enum Category {
+import com.example.manymanyUsers.common.enums.EnumModel;
+
+public enum Category implements EnumModel {
+
+    TEST("테스트");
+
+    private String value;
+
+    Category(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
 }
