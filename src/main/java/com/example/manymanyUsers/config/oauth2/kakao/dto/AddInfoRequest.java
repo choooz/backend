7d6b23ebfff7acaa6f8de23e6d46e702e0591970
chujonghyun <chuwhdgus@gmail.com>
2022-12-22 13:20:19 +0900

    AddInfoRequest DTO 추가

--- src/main/java/com/example/manymanyUsers/config/oauth2/kakao/dto/AddInfoRequest.java
+++ src/main/java/com/example/manymanyUsers/config/oauth2/kakao/dto/AddInfoRequest.java
@@ -1,8 +1,12 @@
 package com.example.manymanyUsers.config.oauth2.kakao.dto;
 
+import com.example.manymanyUsers.vote.enums.Gender;
+import com.example.manymanyUsers.vote.enums.MBTI;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * 유저 정보 추가 기입 DTO
@@ -10,5 +14,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AddInfoRequest {
+
+    private Long userId;
+
+    @NotBlank
+    private MBTI mbti;
+
+    private Integer age;
+
+    @NotBlank
+    private Gender gender;
 
 }
