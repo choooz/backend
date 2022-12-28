baf79c7b568d0252053267f1ab9c853da4fd1194
chujonghyun <chuwhdgus@gmail.com>
2022-12-28 20:27:46 +0900

    addInterestCategoryRequest 추가

---
+++ src/main/java/com/example/manymanyUsers/user/dto/AddInterestCategoryRequest.java
@@ -0,0 +1,10 @@
+package com.example.manymanyUsers.user.dto;
+
+import com.example.manymanyUsers.vote.enums.Category;
+
+public class AddInterestCategoryRequest {
+
+    private Long userId;
+
+    private Category category;
+}
