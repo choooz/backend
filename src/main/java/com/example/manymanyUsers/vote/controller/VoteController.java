7e66203d6ffd32bbddc11241b317fc655b2c7b80
chujonghyun <chuwhdgus@gmail.com>
2022-12-31 23:11:01 +0900

    getVoteList 메서드 추가

--- src/main/java/com/example/manymanyUsers/vote/controller/VoteController.java
+++ src/main/java/com/example/manymanyUsers/vote/controller/VoteController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -40,5 +37,10 @@ public class VoteController {
                 .build();
 
         return new ResponseEntity(createVoteResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity getVoteList() {
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
