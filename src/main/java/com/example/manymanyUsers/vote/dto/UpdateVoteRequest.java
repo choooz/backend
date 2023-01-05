dc685cc8101372147078f142d52fdf8d835ed4b8
wellbeing-dough <kdw990202@naver.com>
2023-01-05 23:04:48 +0900

    vote update dto

---
+++ src/main/java/com/example/manymanyUsers/vote/dto/UpdateVoteRequest.java
@@ -0,0 +1,41 @@
+package com.example.manymanyUsers.vote.dto;
+
+import com.example.manymanyUsers.vote.enums.Age;
+import com.example.manymanyUsers.vote.enums.Category;
+import com.example.manymanyUsers.vote.enums.Gender;
+import com.example.manymanyUsers.vote.enums.MBTI;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class UpdateVoteRequest {
+
+    private Long userId;
+
+    @NotBlank
+    private String title;
+
+    private String ImageA;
+
+    private String ImageB;
+
+    @NotBlank
+    private String detail;
+
+    private Gender FilteredGender;
+
+    private Age FilteredAge;
+
+    private Category category;
+
+    private MBTI FilteredMbti;
+
+    private String titleA;
+
+    private String titleB;
+
+}
