f7fb46c23fa67b6819878e700d49551bbc73c54a
wellbeing-dough <kdw990202@naver.com>
2023-01-07 15:52:54 +0900

    vote update test 메서드 추가

--- src/test/java/com/example/manymanyUsers/service/VoteServiceTest.java
+++ src/test/java/com/example/manymanyUsers/service/VoteServiceTest.java
@@ -6,6 +6,7 @@ import com.example.manymanyUsers.user.dto.SignUpRequest;
 import com.example.manymanyUsers.user.service.UserService;
 import com.example.manymanyUsers.vote.domain.Vote;
 import com.example.manymanyUsers.vote.dto.CreateVoteRequest;
+import com.example.manymanyUsers.vote.dto.UpdateVoteRequest;
 import com.example.manymanyUsers.vote.enums.Age;
 import com.example.manymanyUsers.vote.enums.Category;
 import com.example.manymanyUsers.vote.enums.Gender;
@@ -99,5 +100,73 @@ public class VoteServiceTest {
         //then
         voteService.createVote(createVoteRequest);
 
+    }
+
+    @Test
+    public void 투표수정_성공() throws Exception {
+        //given
+
+        SignUpRequest request = new SignUpRequest("testUser", "test@naver.com", "password", "", "providerId");
+        Long userId = userService.registerUser(request);
+
+        CreateVoteRequest createVoteRequest = new CreateVoteRequest(
+                userId,
+                "투표 제목",
+                "imageA",
+                "imageB",
+                "detailText",
+                Gender.NULL,
+                Age.NULL,
+                Category.NULL,
+                MBTI.ENFJ,
+                "titleA",
+                "titleB");
+
+        voteService.createVote(createVoteRequest);
+
+        Optional<User> userRepositoryByProviderId = userRepository.findById(createVoteRequest.getUserId());
+        User user = userRepositoryByProviderId.get();
+
+        Optional<Vote> byProviderId = voteRepository.findByPostedUser(user);
+        Vote vote = byProviderId.get();
+        System.out.println("수정 전 타이틀: " + vote.getTotalTitle());
+        System.out.println("수정 전 이미지: " + vote.getImageA());
+        System.out.println("수정 전 이미지: " + vote.getImageB());
+        System.out.println("수정 전 타이틀A " + vote.getTitleA());
+        System.out.println("수정 전 타이틀B " + vote.getTitleB());
+
+        //when
+        UpdateVoteRequest updateVoteRequest = new UpdateVoteRequest(
+                vote.getId(),
+                "title, titleA 만 바꾸겠습니다",
+                "imageA",
+                "imageB",
+                "detailText",
+                Gender.NULL,
+                Age.NULL,
+                Category.NULL,
+                MBTI.ENFJ,
+                "title, titleA 만 바꾸겠습니다",
+                "titleB");
+
+        voteService.updateVote(updateVoteRequest);
+
+        //then
+
+        assertEquals(vote.getPostedUser(), user);
+        assertEquals(vote.getTotalTitle(), updateVoteRequest.getTitle());
+        assertEquals(vote.getCategory(), updateVoteRequest.getCategory());
+        assertEquals(vote.getDetail(), updateVoteRequest.getDetail());
+        assertEquals(vote.getImageA(), updateVoteRequest.getImageA());
+        assertEquals(vote.getImageB(), updateVoteRequest.getImageB());
+        assertEquals(vote.getFilteredGender(), updateVoteRequest.getFilteredGender());
+        assertEquals(vote.getFilteredAge(), updateVoteRequest.getFilteredAge());
+        assertEquals(vote.getCategory(), updateVoteRequest.getCategory());
+
+        System.out.println("수정 후 타이틀: " + vote.getTotalTitle());
+        System.out.println("수정 후 이미지: " + vote.getImageA());
+        System.out.println("수정 후 이미지: " + vote.getImageB());
+        System.out.println("수정 후 타이틀A " + vote.getTitleA());
+        System.out.println("수정 후 타이틀B " + vote.getTitleB());
     }
 }
