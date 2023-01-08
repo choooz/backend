4ded21c1ffe182acbbb4eece527412e28bbfa4b6
chujonghyun <chuwhdgus@gmail.com>
2023-01-08 18:24:47 +0900

    리스트 조회 기능 테스트 코드 작성중

--- src/test/java/com/example/manymanyUsers/service/VoteServiceTest.java
+++ src/test/java/com/example/manymanyUsers/service/VoteServiceTest.java
@@ -7,20 +7,24 @@ import com.example.manymanyUsers.user.enums.Providers;
 import com.example.manymanyUsers.user.service.UserService;
 import com.example.manymanyUsers.vote.domain.Vote;
 import com.example.manymanyUsers.vote.dto.CreateVoteRequest;
-import com.example.manymanyUsers.vote.enums.Age;
-import com.example.manymanyUsers.vote.enums.Category;
-import com.example.manymanyUsers.vote.enums.Gender;
-import com.example.manymanyUsers.vote.enums.MBTI;
+import com.example.manymanyUsers.vote.dto.VoteListData;
+import com.example.manymanyUsers.vote.enums.*;
 import com.example.manymanyUsers.vote.repository.VoteRepository;
 import com.example.manymanyUsers.vote.service.VoteService;
 import javassist.NotFoundException;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Slice;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
@@ -102,12 +106,106 @@ public class VoteServiceTest {
 
     }
 
-//    @Test
-//    public void 투표리스트_조회_성공() throws Exception {
-//        //given
-//
-//        //when
-//
-//        //then
-//    }
+    @Test
+    public void 투표리스트_조회_성공() throws Exception {
+        //given
+        List<Vote> voteTestList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            // 유저 생성
+            String name = "name" + i;
+            String email = "email" + i;
+            Providers provider;
+            switch (i%3) {
+                case 0:
+                    provider = Providers.KAKAO;
+                    email += "@kakao.com";
+                    break;
+                case 1 :
+                    provider = Providers.NAVER;
+                    email += "@naver.com";
+                    break;
+                case 2 :
+                    provider = Providers.GOOGLE;
+                    email += "@google.com";
+                    break;
+                default:
+                    provider = Providers.KAKAO;
+                    break;
+            }
+
+            SignUpRequest signUpRequest = new SignUpRequest(name, email, "", provider, "");
+            Long userid;
+            try {
+                userid = userService.registerUser(signUpRequest);
+
+                // 투표 생성
+                String voteTitle = "title " + i;
+                String imageA = "https://mblogthumb-phinf.pstatic.net/MjAxODA0MjRfNDYg/MDAxNTI0NTQ5MDc3MDU1.iF25cKVnG3Ae8BkD20IIoB5U1vlOcN3kXWt7XjU0jR8g.1yRmaxjdNtOgATk4gnZ_cr4WnoBauqUgoE0yg0q4QWog.JPEG.beelike1115/image_5370526891524549066762.jpg?type=w800";
+                String imageB = "https://mblogthumb-phinf.pstatic.net/MjAxODA0MjRfNDYg/MDAxNTI0NTQ5MDc3MDU1.iF25cKVnG3Ae8BkD20IIoB5U1vlOcN3kXWt7XjU0jR8g.1yRmaxjdNtOgATk4gnZ_cr4WnoBauqUgoE0yg0q4QWog.JPEG.beelike1115/image_5370526891524549066762.jpg?type=w800";
+                String detail = "디테일 텍스트" + i;
+                Gender filteredGender = Gender.NULL;
+                switch (i%2){
+                    case 1  : filteredGender = Gender.FEMALE;
+                        break;
+                    case 2 : filteredGender = Gender.MALE;
+                        break;
+                    default:
+                        break;
+                }
+                Age filteredAge = Age.NULL;
+                Category category = Category.NULL;
+                switch (i%5){
+                    case 1: category = Category.CARRIER;
+                        break;
+                    case 2: category = Category.FOODS;
+                        break;
+                    case 3: category = Category.LOVE;
+                        break;
+                    case 4: category = Category.FASHION;
+                        break;
+                    case 5: category = Category.INTEREST;
+                    default: break;
+                }
+                MBTI filteredMbti = MBTI.ENFJ;
+                switch (i % 4) {
+                    case 1 : filteredMbti = MBTI.ENFP;
+                        break;
+                    case 2 : filteredMbti = MBTI.ENTJ;
+                        break;
+                    case 3 : filteredMbti = MBTI.INFP;
+                        break;
+                    case 4 : filteredMbti = MBTI.ISFJ;
+                    default: break;
+                }
+                String titleA = "titleA" + i;
+                String titleB = "titleB" + i;
+                CreateVoteRequest createVoteRequest = new CreateVoteRequest(userid, voteTitle, imageA, imageB, detail, filteredGender, filteredAge, category, filteredMbti, titleA, titleB);
+                Vote vote = voteService.createVote(createVoteRequest);
+                voteTestList.add(vote);
+            } catch (Exception e) {
+                System.out.println("e = " + e);
+            }
+        }
+        //when
+        Slice<VoteListData> voteSlice = voteService.getVoteList(SortBy.ByTime, 0, 10);
+        List<VoteListData> voteResultList = voteSlice.getContent();
+        //then
+        voteTestList.sort((v1,v2)-> {
+            if (v1.getCreatedDate().isBefore(v2.getCreatedDate())) {
+                return 1;
+            } else if (v1.getCreatedDate().isAfter(v2.getCreatedDate())) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        for (VoteListData voteListData : voteResultList) {
+            int i =0;
+            System.out.println("voteListData.getVoteId() = " + voteListData.getVoteId());
+            System.out.println("voteTestList.get(i).getId() = " + voteTestList.get(i).getId());
+            System.out.println("i = " + i);
+            assertEquals(voteListData.getVoteId(),voteTestList.get(i).getId());
+            i++;
+        }
+    }
 }
