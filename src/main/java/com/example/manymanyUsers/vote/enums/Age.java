53f406540a562f27ef84fe055e48a93ed270fd2f
chujonghyun <chuwhdgus@gmail.com>
2023-01-11 23:25:16 +0900

    Age 나이대 추가

--- src/main/java/com/example/manymanyUsers/vote/enums/Age.java
+++ src/main/java/com/example/manymanyUsers/vote/enums/Age.java
@@ -3,7 +3,11 @@ package com.example.manymanyUsers.vote.enums;
 import com.example.manymanyUsers.common.enums.EnumModel;
 
 public enum Age implements EnumModel {
-    TEST("테스트"),
+    teenager("10대"),
+    twenties("20대"),
+    thirties("30대"),
+    tourties("40대"),
+    fifties("50대"),
     NULL("null");
 
     private String value;
