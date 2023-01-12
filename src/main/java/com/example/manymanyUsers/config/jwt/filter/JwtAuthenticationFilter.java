4e5923c6d027bcd37cd3fae0b1a79459fa195975
wellbeing-dough <kdw990202@naver.com>
2023-01-13 02:56:26 +0900

    jwt url 추가

--- src/main/java/com/example/manymanyUsers/config/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/manymanyUsers/config/jwt/filter/JwtAuthenticationFilter.java
@@ -35,7 +35,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
         String baseUrl = "http://localhost:8080/api";
         StringBuffer requestURL = request.getRequestURL();
-        if(requestURL.toString().equals(baseUrl + "/oauth/login") || requestURL.toString().equals(baseUrl + "/user/addInfo") || requestURL.toString().equals(baseUrl + "/vote/createVote")){
+        if(requestURL.toString().equals(baseUrl + "/oauth/login") || requestURL.toString().equals(baseUrl + "/user/addInfo")
+                || requestURL.toString().equals(baseUrl + "/vote/createVote") || requestURL.toString().equals(baseUrl + "/updateVote")
+                || requestURL.toString().equals(baseUrl + "/deleteVote/{voteId}")) {
             String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
                 try {
                     HashMap<String, Object> parseJwtTokenMap = jwtTokenProvider.parseJwtToken(authorizationHeader);
