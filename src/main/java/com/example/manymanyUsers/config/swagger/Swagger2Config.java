46154c1b98ecb26ca08b4f9dbcf74b0a76f0b318
chujonghyun <chuwhdgus@gmail.com>
2023-01-14 23:55:17 +0900

    GetKakaoToken 스웨거 설명 추가

--- src/main/java/com/example/manymanyUsers/config/swagger/Swagger2Config.java
+++ src/main/java/com/example/manymanyUsers/config/swagger/Swagger2Config.java
@@ -3,6 +3,8 @@ package com.example.manymanyUsers.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +21,28 @@ public class Swagger2Config {
     }
     @Bean
     public OpenAPI springShopOpenAPI() {
+
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAuth";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+
         return new OpenAPI()
-                .components(new Components())
                 .info(new Info().title("ManyUser Project API")
                         .description("프로젝트 API 명세서입니다.")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 
 }
