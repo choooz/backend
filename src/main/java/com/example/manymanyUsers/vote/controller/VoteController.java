84dd884cd7dbe43e89f6e0afdb27fc0a3edf51fe
chujonghyun <chuwhdgus@gmail.com>
2023-01-18 15:11:53 +0900

    GetVoteList category 파라미터 required=false 옵션 추가

--- src/main/java/com/example/manymanyUsers/vote/controller/VoteController.java
+++ src/main/java/com/example/manymanyUsers/vote/controller/VoteController.java
@@ -49,7 +49,7 @@ public class VoteController {
 
     @Operation(description = "투표 리스트 조회")
     @GetMapping("/")
-    public ResponseEntity<GetVoteListResponse> getVoteList(@RequestParam SortBy sortBy, @RequestParam int page, @RequestParam int size, @RequestParam Category category) {
+    public ResponseEntity<GetVoteListResponse> getVoteList(@RequestParam SortBy sortBy, @RequestParam int page, @RequestParam int size, @RequestParam(required = false) Category category) {
         Slice<VoteListData> voteListData = voteService.getVoteList(sortBy, page, size, category);
         GetVoteListResponse voteResponse = GetVoteListResponse.builder()
                 .voteSlice(voteListData)
