c8923ab9e88bb8b85ac28c7beec3fb69696ec72a
wellbeing-dough <kdw990202@naver.com>
2023-01-24 21:56:03 +0900

    예외처리 ExceptionMessage 생성

---
+++ src/main/java/com/example/manymanyUsers/exception/ExceptionMessage.java
@@ -0,0 +1,22 @@
+package com.example.manymanyUsers.exception;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExceptionMessage {
+    private StatusEnum status;
+    private String msg;
+
+    public static ExceptionMessage of(StatusEnum status, String message){
+        return ExceptionMessage.builder().status(status)
+                .msg(message)
+                .build();
+    }
+
+    public static ExceptionMessage of(String msg){
+        return ExceptionMessage.of(StatusEnum.BAD_REQUEST,msg);
+    }
+}
