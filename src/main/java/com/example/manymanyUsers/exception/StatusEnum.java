d2c8dc2e6a680d1da7037b7667572caa176ee19c
wellbeing-dough <kdw990202@naver.com>
2023-01-24 21:56:20 +0900

    예외처리 StatusEnum 생성

---
+++ src/main/java/com/example/manymanyUsers/exception/StatusEnum.java
@@ -0,0 +1,16 @@
+package com.example.manymanyUsers.exception;
+
+public enum StatusEnum {
+    BAD_REQUEST(400, "BAD_REQUEST"),
+    USER_NOT_FOUND(404,"USER_NOT_FOUND"),
+    VOTE_NOT_FOUND(200,"VOTE_NOT_FOUND");
+
+    final int statusCode;
+    final String code;
+
+    StatusEnum(int statusCode, String code) {
+        this.statusCode = statusCode;
+        this.code = code;
+    }
+}
+
