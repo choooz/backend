1981d988fb2567a8c4e5714ab109dabdd40d1952
wellbeing-dough <kdw990202@naver.com>
2023-01-25 03:41:02 +0900

    ################  # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 feat : 예외처리 분리
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - 불필요한 주석 제거
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    CZ-272
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- src/main/java/com/example/manymanyUsers/vote/controller/VoteController.java
+++ src/main/java/com/example/manymanyUsers/vote/controller/VoteController.java
@@ -38,23 +38,6 @@ public class VoteController {
                 .build();
         return new ResponseEntity(createVoteResponse, HttpStatus.OK);
 
-
-//        try {
-//            Long voteId = voteService.createVote(createVoteRequest, longId);
-//
-//            CreateVoteResponse createVoteResponse = CreateVoteResponse.builder()
-//                    .voteId(voteId)
-//                    .message("투표 생성에 성공했습니다.")
-//                    .build();
-//
-//            return new ResponseEntity(createVoteResponse, HttpStatus.OK);
-//        } catch (NotFoundException e) {
-//            log.info("error", e);
-//            CreateVoteResponse createVoteResponse = CreateVoteResponse.builder()
-//                    .message("해당 아이디를 가진 유저가 없습니다. 아이디를 다시 확인하세요.")
-//                    .build();
-//            return new ResponseEntity(createVoteResponse, HttpStatus.NOT_FOUND);
-//        }
     }
 
     @Operation(description = "투표 리스트 조회")
@@ -96,21 +79,5 @@ public class VoteController {
 
         return new ResponseEntity(updateVoteResponse, HttpStatus.OK);
 
-
-//        try {
-//            voteService.deleteVote(voteId, longId);
-//        } catch (NotFoundException e) {
-//            log.info("error",e);
-//            CommonResponse createVoteResponse = CommonResponse.builder()
-//                    .message("해당 아이디를 가진 투표가 없습니다. 아이디를 다시 확인하세요.")
-//                    .build();
-//            return new ResponseEntity(createVoteResponse, HttpStatus.NOT_FOUND);
-//        }
-//
-//        CommonResponse updateVoteResponse = CommonResponse.builder()
-//                .message("투표 삭제에 성공했습니다")
-//                .build();
-//
-//        return new ResponseEntity(updateVoteResponse, HttpStatus.OK);
     }
 }
