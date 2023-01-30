23f86195e67d9d8a388490de3cf09c91d3737cd3
wellbeing-dough <kdw990202@naver.com>
2023-01-31 03:32:33 +0900

    ExceptionMessage @Getter 어노테이션 추가 response 로 반환안되던 이유 핵심 해결

--- src/main/java/com/example/manymanyUsers/exception/ExceptionMessage.java
+++ src/main/java/com/example/manymanyUsers/exception/ExceptionMessage.java
@@ -4,17 +4,18 @@ import lombok.*;
 
 @AllArgsConstructor
 @Builder
+@Getter
 public class ExceptionMessage {
     private StatusEnum status;
     private String message;
 
-    public static ExceptionMessage of(StatusEnum status, String message){
+    public static ExceptionMessage of(StatusEnum status, String message) {
         return ExceptionMessage.builder().status(status)
                 .message(message)
                 .build();
     }
 
-    public static ExceptionMessage of(String msg){
-        return ExceptionMessage.of(StatusEnum.BAD_REQUEST,msg);
+    public static ExceptionMessage of(String msg) {
+        return ExceptionMessage.of(StatusEnum.BAD_REQUEST, msg);
     }
 }
