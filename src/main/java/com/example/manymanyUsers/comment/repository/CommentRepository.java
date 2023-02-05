48f5467e0285b7c09be1abe2726d277a462d3c38
alsduq1117 <alsduq1117@naver.com>
2023-02-05 22:20:16 +0900

    [CZ-289] style: Query 키워드 -> 대문자, 테이블,칼럼 -> 소문자로 수정

--- src/main/java/com/example/manymanyUsers/comment/repository/CommentRepository.java
+++ src/main/java/com/example/manymanyUsers/comment/repository/CommentRepository.java
@@ -22,27 +22,27 @@ public interface CommentRepository extends JpaRepository<Comment,Long> {
     //댓글 필터로 거르고 댓글에 속하는 대댓글 모두 가져오는 쿼리
     @Query("SELECT c FROM Comment c " +
             "LEFT JOIN c.parent p " +
-            "where (c.voteId= :voteId and c.parent is null and (:gender is null or c.gender = :gender) and (:age is null or c.age = :age) and (:mbti is null or c.mbti = :mbti)) " +
-            "or (c.voteId= :voteId and c.parent is not null and (:gender is null or p.gender = :gender) and (:age is null or p.age = :age) and (:mbti is null or p.mbti = :mbti))" +
-            "order by p.id ASC NULLS FIRST, c.id ASC")
-    List<Comment> findCommentsByfilterAndGetChildren(@Param("voteId") Long voteId, @Param("gender") Gender gender, @Param("age") Age age, @Param("mbti") MBTI mbti);
+            "WHERE (c.voteId= :voteId AND c.parent IS NULL AND (:gender IS NULL OR c.gender = :gender) AND (:age IS NULL OR c.age = :age) AND (:mbti IS NULL OR c.mbti = :mbti)) " +
+            "OR (c.voteId= :voteId AND c.parent is not null AND (:gender IS NULL OR p.gender = :gender) AND (:age IS NULL OR p.age = :age) AND (:mbti IS NULL OR p.mbti = :mbti))" +
+            "ORDER BY p.id ASC NULLS FIRST, c.id ASC")
+    List<Comment> findCommentsByfilterANDGetChildren(@Param("voteId") Long voteId, @Param("gender") Gender gender, @Param("age") Age age, @Param("mbti") MBTI mbti);
 
     //댓글,대댓글 모두 필터에 걸러지는 쿼리
     @Query("SELECT c FROM Comment c " +
             "LEFT JOIN c.parent p " +
-            "where (c.voteId= :voteId and c.parent is null and (:gender is null or c.gender = :gender) and (:age is null or c.age = :age) and (:mbti is null or c.mbti = :mbti)) " +
-            "or (c.voteId= :voteId and c.parent is not null and (:gender is null or ((p.gender = :gender) and (c.gender = :gender))) and (:age is null or ((p.age = :age) and (c.age = :age))) and (:mbti is null or ((p.mbti = :mbti) and (c.mbti = :mbti))))" +
-            "order by p.id ASC NULLS FIRST, c.id ASC")
+            "WHERE (c.voteId= :voteId AND c.parent IS NULL AND (:gender IS NULL OR c.gender = :gender) AND (:age IS NULL OR c.age = :age) AND (:mbti IS NULL OR c.mbti = :mbti)) " +
+            "OR (c.voteId= :voteId AND c.parent is not null AND (:gender IS NULL OR ((p.gender = :gender) AND (c.gender = :gender))) AND (:age IS NULL OR ((p.age = :age) AND (c.age = :age))) AND (:mbti IS NULL OR ((p.mbti = :mbti) AND (c.mbti = :mbti))))" +
+            "ORDER BY p.id ASC NULLS FIRST, c.id ASC")
     List<Comment> findCommentsAllByfilter(@Param("voteId") Long voteId, @Param("gender") Gender gender, @Param("age") Age age, @Param("mbti") MBTI mbti);
 
     
     @Query("SELECT c FROM Comment c" +
-            " WHERE c.voteId = :voteId and c.parent is null and (:gender is null or c.gender = :gender) and (:age is null or c.age = :age) and (:mbti is null or c.mbti = :mbti)" +
+            " WHERE c.voteId = :voteId AND c.parent IS NULL AND (:gender IS NULL OR c.gender = :gender) AND (:age IS NULL OR c.age = :age) AND (:mbti IS NULL OR c.mbti = :mbti)" +
             " ORDER BY (c.likeCount + c.hateCount) DESC , c.createdDate DESC")
     List<Comment> findHotComments(@Param("voteId") Long voteId,@Param("gender") Gender gender, @Param("age") Age age, @Param("mbti") MBTI mbti,Pageable pageable);
 
     @Query("SELECT c FROM Comment c" +
-            " WHERE c.voteId = :voteId and c.parent is null and (:gender is null or c.gender = :gender) and (:age is null or c.age = :age) and (:mbti is null or c.mbti = :mbti) " +
+            " WHERE c.voteId = :voteId AND c.parent IS NULL AND (:gender IS NULL OR c.gender = :gender) AND (:age IS NULL OR c.age = :age) AND (:mbti IS NULL OR c.mbti = :mbti) " +
             "ORDER BY c.createdDate DESC")
     List<Comment> findNewestComments(@Param("voteId") Long voteId, @Param("gender") Gender gender, @Param("age") Age age, @Param("mbti") MBTI mbti, Pageable pageable);
 
