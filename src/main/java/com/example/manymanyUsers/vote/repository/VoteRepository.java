4f2e0cb5dcff96b8c299a3bc67565c2330b64f61
alsduq1117 <alsduq1117@naver.com>
2023-02-22 02:47:58 +0900

    fix: import error 수정

--- src/main/java/com/example/manymanyUsers/vote/repository/VoteRepository.java
+++ src/main/java/com/example/manymanyUsers/vote/repository/VoteRepository.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
