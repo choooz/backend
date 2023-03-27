c7ab3c922e1af7563afda489bcd20d683f8961b8
wellbeing-dough <kdw990202@naver.com>
2023-03-28 02:16:17 +0900

    refact : TokenController swagger 문서 리펙토
    CZ-496

--- src/main/java/com/example/manymanyUsers/token/controller/TokenController.java
+++ src/main/java/com/example/manymanyUsers/token/controller/TokenController.java
@@ -3,6 +3,8 @@ package com.example.manymanyUsers.token.controller;
 import com.example.manymanyUsers.token.TokenResponse;
 import com.example.manymanyUsers.token.enums.TokenType;
 import com.example.manymanyUsers.token.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/token")
+@Tag(name = "token", description = "토큰 재발급 api")
 public class TokenController {
 
     private final TokenService tokenService;
 
+    @Operation(summary = "토큰 재발급", description = "enum 형태로 TokenType, String 타입으로 refreshToken 형태로 보내주시면 됩니다")
     @GetMapping("")
     public ResponseEntity<TokenResponse> reissueToken(TokenType tokenType, String refreshToken) {
         if(tokenType == TokenType.ACCESS){
