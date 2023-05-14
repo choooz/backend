b87b970756895d5d0ab8e5d22c00e9ca8bb0157a
jonghyunhub <chuwhdgus@gmail.com>
2023-05-14 21:32:33 +0900

    chooz-api build setting

---
+++ chooz-api/src/main/java/com/chooz/ChoozApplication.java
@@ -0,0 +1,12 @@
+package com.chooz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoozApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoozApplication.class, args);
+    }
+}
