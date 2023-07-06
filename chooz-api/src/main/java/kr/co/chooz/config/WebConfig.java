7f145d98c25b3d65039ac9f46fa799ed7264f002
jonghyunhub <chuwhdgus@gmail.com>
2023-07-07 01:47:37 +0900

    feat : cors 세팅 추가

--- chooz-api/src/main/java/kr/co/chooz/config/WebConfig.java
+++ chooz-api/src/main/java/kr/co/chooz/config/WebConfig.java
@@ -6,10 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
     }
 }
