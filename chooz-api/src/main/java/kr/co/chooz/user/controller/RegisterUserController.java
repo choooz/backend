b107512308a6dfcbfbcb4471cf7b98a5293771b5
jonghyunhub <chuwhdgus@gmail.com>
2023-07-14 00:59:11 +0900

    feat : jwt 필터 추가

--- chooz-api/src/main/java/kr/co/chooz/user/controller/RegisterUserController.java
+++ chooz-api/src/main/java/kr/co/chooz/user/controller/RegisterUserController.java
@@ -1,5 +1,6 @@
 package kr.co.chooz.user.controller;
 
+import io.jsonwebtoken.Claims;
 import kr.co.chooz.user.dto.LoginToken;
 import kr.co.chooz.user.dto.SocialLoginRequest;
 import kr.co.chooz.user.dto.TokenResponse;
@@ -7,16 +8,13 @@ import kr.co.chooz.user.port.in.UserUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class RegisterUserController {
     private final UserUseCase userUserCase;
 
@@ -24,5 +22,10 @@ public class RegisterUserController {
     public ResponseEntity<TokenResponse> kakaoLogin(@Valid @RequestBody SocialLoginRequest socialLoginRequest) {
         LoginToken loginToken = userUserCase.signupByThirdParty(socialLoginRequest.toKakaoDomain());
         return new ResponseEntity<>(new TokenResponse(loginToken), HttpStatus.OK);
+    }
+
+    @GetMapping("/userInfo")
+    public ResponseEntity<Claims> getUserInfo(@RequestAttribute Claims claims) {
+        return new ResponseEntity<>(claims, HttpStatus.OK);
     }
 }
